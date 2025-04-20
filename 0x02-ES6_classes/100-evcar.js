07f8e4652efa195363536ea70937547f0485086e
EugineI <eugineindimuli@gmail.com>
2025-04-20 17:34:10 +0300

    this.constructor

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,13 @@
-import Car from './10-car'
+import Car from './10-car';
 
 export default class EVCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-		this._range = range;
-	}
-	cloneCar() {
-		return new Car(undefined, undefined, undefined);
-	}
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    this._brand = undefined;
+    return new Car(this.brand, undefined, undefined);
+  }
 }
