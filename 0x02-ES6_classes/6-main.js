fb4ac8aa1f0ae3d558b4e26968d9aa55746ae868
EugineI <eugineindimuli@gmail.com>
2025-04-20 17:06:56 +0300

    classes

---
+++ 0x02-ES6_classes/6-main.js
@@ -0,0 +1,6 @@
+import SkyHighBuilding from './6-sky_high.js';
+
+const building = new SkyHighBuilding("Three", 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
