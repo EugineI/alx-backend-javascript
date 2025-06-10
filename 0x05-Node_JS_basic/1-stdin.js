a41785ac45b27947d62e86e0c9281b899258f363
EugineI <eugineindimuli@gmail.com>
2025-06-10 21:30:32 +0300

    1-stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,25 @@
 #!/usr/bin/node
+
 /**
  * This script demonstrates basic stdin/stdout interaction
  * It asks for a name and displays it back
  */
-process.stdout.write('Welcome to ALX School, what is your name?\n');
+function main() {
+  process.stdout.write('Welcome to ALX, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
+  process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    process.stdout.write(`Your name is: ${input}\n`);
+    process.exit(0);
+  });
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
-});
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+module.exports = main;
+
+if (require.main === module) {
+  main();
+}
