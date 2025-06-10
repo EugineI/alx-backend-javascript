77866dd555d6ae393fefce2d09b7a7cf69a55c27
EugineI <eugineindimuli@gmail.com>
2025-06-10 21:26:33 +0300

    1-stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,12 +3,14 @@
  * This script demonstrates basic stdin/stdout interaction
  * It asks for a name and displays it back
  */
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to ALX School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
