f1471d35fe6a8cee132d0580d6a4aad97b8ee5a6
EugineI <eugineindimuli@gmail.com>
2025-06-11 11:22:41 +0300

    lint4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,14 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello ALX!');
+  if (req.url === '/' || req.url === '/test') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello ALX!');
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
 app.listen(1245);
