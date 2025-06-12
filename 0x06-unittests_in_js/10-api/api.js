56073ffe9aa3ff49b9a0b458671984a786af2432
EugineI <eugineindimuli@gmail.com>
2025-06-13 00:07:14 +0300

    npm test

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,13 +3,11 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = 7865;
-
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
+app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 
@@ -24,11 +22,15 @@ app.get('/available_payments', (req, res) => {
 
 app.post('/login', (req, res) => {
   const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+  if (userName) {
+    res.send(`Welcome ${userName}`);
+  } else {
+    res.status(400).send('Missing userName');
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,77 +1,67 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api');
-
-chai.use(chaiHttp);
-const { expect } = chai;
+const request = require('request');
+const { expect } = require('chai');
 
 describe('Index page', () => {
-  it('should return status 200', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+  const url = 'http://localhost:7865';
+
+  it('Correct status code?', (done) => {
+    request.get(url, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('should return correct message', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+  it('Correct result?', (done) => {
+    request.get(url, (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
 
 describe('Cart page', () => {
-  it('should return 200 for numeric id', (done) => {
-    chai.request(app)
-      .get('/cart/12')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.equal('Payment methods for cart 12');
-        done();
-      });
+  it('Correct status code when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/12', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
   });
 
-  it('should return 404 for non-numeric id', (done) => {
-    chai.request(app)
-      .get('/cart/abc')
-      .end((err, res) => {
-        expect(res).to.have.status(404);
-        done();
-      });
+  it('Correct status code when :id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
   });
 });
 
 describe('/available_payments endpoint', () => {
-  it('should return correct payment methods object', (done) => {
-    chai.request(app)
-      .get('/available_payments')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          payment_methods: {
-            credit_cards: true,
-            paypal: false
-          }
-        });
-        done();
+  it('Returns correct payment methods object', (done) => {
+    request.get('http://localhost:7865/available_payments', { json: true }, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
       });
+      done();
+    });
   });
 });
 
 describe('/login endpoint', () => {
-  it('should return welcome message with userName', (done) => {
-    chai.request(app)
-      .post('/login')
-      .send({ userName: 'Betty' })
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.equal('Welcome Betty');
-        done();
-      });
+  it('Returns correct welcome message', (done) => {
+    const options = {
+      url: 'http://localhost:7865/login',
+      method: 'POST',
+      json: { userName: 'Betty' }
+    };
+    request(options, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome Betty');
+      done();
+    });
   });
 });
